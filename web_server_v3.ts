ba2c015df1c0134cb648eed7ec8f5b827addda17
Tafajjul Khan <tafajjulk607@gmail.com>
2026-04-10 18:09:37 +0530

    A simple network protocol

---
+++ web_server_v3.ts
@@ -0,0 +1,181 @@
+import * as net from "net";
+
+type TCPConn = {
+  // the socket object
+  socket: net.Socket;
+  // from the "error" event
+  err: null | Error;
+  // EOF from the 'end' event
+  ended: boolean;
+  // the callback of the promise of the current read
+  reader: null | {
+    resolve: (value: Buffer) => void;
+    reject: (reason: Error) => void;
+  };
+};
+
+function soInit(socket: net.Socket): TCPConn {
+  const conn: TCPConn = {
+    socket: socket,
+    err: null,
+    ended: false,
+    reader: null,
+  };
+  socket.on("data", (data: Buffer) => {
+    if (!conn.reader) return;
+    // pause the "data" event until the next read
+    conn.socket.pause();
+    // fulfill the promise of the current read
+    conn.reader.resolve(data);
+    conn.reader = null;
+  });
+  socket.on("end", () => {
+    conn.ended = true;
+    if (conn.reader) {
+      conn.reader.resolve(Buffer.from("")); // EOF
+      conn.reader = null;
+    }
+  });
+  socket.on("error", (err: Error) => {
+    // errors are also delivered to the current read
+    conn.err = err;
+    if (conn.reader) {
+      conn.reader.reject(err);
+      conn.reader = null;
+    }
+  });
+  return conn;
+}
+
+// returns an empty "buffers" after EOF
+
+function soRead(conn: TCPConn): Promise<Buffer> {
+  console.assert(!conn.reader); // no concurrent calls
+  return new Promise((resolve, reject) => {
+    if (conn.err) {
+      reject(conn.err);
+      return;
+    }
+    if (conn.ended) {
+      resolve(Buffer.from("")); // eof
+      return;
+    }
+    // save the promise callbacks
+    conn.reader = { resolve: resolve, reject: reject };
+    // and resume the 'data' event to fulfill the promise later
+    conn.socket.resume();
+  });
+}
+
+function soWrite(conn: TCPConn, data: Buffer): Promise<void> {
+  console.assert(data.length > 0);
+  return new Promise((resolve, reject) => {
+    if (conn.err) {
+      reject(conn.err);
+      return;
+    }
+    conn.socket.write(data, (err?: Error | null) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+async function newConn(socket: net.Socket): Promise<void> {
+  console.log("new connection", socket.remoteAddress, socket.remotePort);
+  try {
+    await serveClient(socket);
+  } catch (exc) {
+    console.error("exception:", exc);
+  } finally {
+    socket.destroy();
+  }
+}
+async function serveClient(socket: net.Socket): Promise<void> {
+  const conn: TCPConn = soInit(socket);
+  const buf: DynBuf = { data: Buffer.alloc(0), length: 0 };
+  while (true) {
+    // try to get 1 message from the buffer
+    const msg: null | Buffer = cutMessage(buf);
+    console.log("Buffer length:", buf.length);
+    if (!msg) {
+      console.log("waiting for more data....");
+      // need more data
+      const data: Buffer = await soRead(conn);
+      console.log("Data received:", data.toString());
+      bufPush(buf, data);
+      // EOF
+      console.log("data", data);
+      if (data.length === 0) {
+        // ommitted
+        console.log("client closed connection");
+        return;
+      }
+      // got some data try it again
+      continue;
+    }
+    const msgString = msg.toString().trim();
+
+    // if (msg.equals(Buffer.from("quit\r\n"))) for windows \r becouse its return quit\r\n
+
+    if (msgString === "quit") {
+      await soWrite(conn, Buffer.from("Bye.\n"));
+      socket.destroy();
+      return;
+    } else {
+      const reply = Buffer.concat([Buffer.from("Echo: "), msg]);
+      await soWrite(conn, reply);
+    }
+  }
+} // loop for the message
+
+type DynBuf = {
+  data: Buffer;
+  length: number;
+};
+
+// extract a message from the buffer, return null if not enough data
+function cutMessage(buf: DynBuf): null | Buffer {
+  const idx = buf.data.subarray(0, buf.length).indexOf("\n");
+  if (idx < 0) {
+    return null; // not complete
+  }
+  // make a copy of the message and move the reamining data to the front
+  const msg = Buffer.from(buf.data.subarray(0, idx + 1));
+  bufPop(buf, idx + 1);
+  return msg;
+}
+
+// append data to dybuf
+function bufPush(buf: DynBuf, data: Buffer): void {
+  const newLen = buf.length + data.length;
+  if (buf.data.length < newLen) {
+    // grow the capicity...
+    let cap = Math.max(buf.data.length, 32);
+    while (cap < newLen) {
+      cap *= 2;
+    }
+    const grown = Buffer.alloc(cap);
+    buf.data.copy(grown, 0, 0);
+    buf.data = grown;
+  }
+  data.copy(buf.data, buf.length, 0);
+  buf.length = newLen;
+}
+
+function bufPop(buf: DynBuf, len: number): void {
+  buf.data.copyWithin(0, len, buf.length);
+  buf.length -= len;
+}
+
+const server = net.createServer({
+  pauseOnConnect: true, // required TCPConn
+});
+
+server.on("connection", newConn); 
+server.listen({ host: "127.0.0.1", port: 1234 }, () => {
+  console.log("Server chalu ho gaya: 127.0.0.1:1234");
+});
